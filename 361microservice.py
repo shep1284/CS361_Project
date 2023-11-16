e2cf19560349851371b85f923ab7d22a9076ce87
unknown <shepherf@oregonstate.edu>
2023-11-15 19:37:42 -0800

    changed to martian date

--- 361microservice.py
+++ 361microservice.py
@@ -5,10 +5,8 @@ import random
 rovers = ["Curiosity", "Opportunity", "Spirit", "Perseverance"]
 
 def generate_random_query():
-    # Generate a random Martian date between January 4, 2004, and today
-    min_date = "2004-01-04"
-    max_date = "2023-10-26"  # Update with the current date
-    random_date = f"{random.randint(2004, 2023)}-{random.randint(1, 12):02d}-{random.randint(1, 28):02d}"
+    # Generate a random Martian date (integer)
+    random_date = random.randint(1, 10000)
 
     # Randomly select a rover
     selected_rover = random.choice(rovers)
